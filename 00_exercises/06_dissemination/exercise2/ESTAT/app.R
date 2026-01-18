b9eb5f6d87fa3acbd9c6556142d2b8a07d76512f
lidiyamishieva <lidiya.mishieva@mail.de>
2026-01-18 23:20:22 +0100

    implemeted the selection of data based on nuts level

--- 00_exercises/06_dissemination/exercise2/ESTAT/app.R
+++ 00_exercises/06_dissemination/exercise2/ESTAT/app.R
@@ -1,3 +1,4 @@
+library(tidyverse)
 library(shiny)
 library(eurostat)
 library(giscoR)
@@ -5,19 +6,18 @@ library(giscoR)
 
 ui <- fluidPage(
   
-    # app title  
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("ADD TITLE"),
 
     sidebarLayout(
+      
+      # input
       sidebarPanel(
-        
-        selectInput(
+        # checkbox
+        radioButtons(
           inputId = "selected_nuts_level",
           label = "Select NUTS level:",
-          choices = c("NUTS 1", "NUTS 2", "NUTS 3"),
-          selected = "NUTS 1"
-          ),
-        
+          choices = c("NUTS 1", "NUTS 2", "NUTS 3")),
+        # dropdown
         selectInput(
           inputId = "selected_var",
           label = "Select a Eurostat dataset:",
@@ -35,7 +35,27 @@ ui <- fluidPage(
     )
 )
 
-server <- function(input, output) {}
+eurostat_database <- get_eurostat_toc() 
 
-# run the application 
+server <- function(input, output, session) {
+  
+  # workflow to update the choice of data based on the selected nuts level
+  observeEvent(input$selected_nuts_level, {
+    
+    filtered_datasets <- eurostat_database %>%
+      filter(type %in% c("table", "dataset")) %>%
+      filter(str_detect(title, regex(input$selected_nuts_level, ignore_case = TRUE))
+      )
+    
+    choices <- filtered_datasets$title
+    
+    updateSelectInput(
+      session,
+      inputId = "selected_var",
+      choices = sort(choices),
+      selected = ""
+    )
+  })
+}
+
 shinyApp(ui = ui, server = server)
