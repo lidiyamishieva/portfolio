0e356f2a4321d10bdfbfedb78eb22b5a4ae7b166
lidiyamishieva <lidiya.mishieva@mail.de>
2026-01-19 01:46:39 +0100

    adapt selection process add plot

--- 00_exercises/06_dissemination/exercise2/ESTAT/app.R
+++ 00_exercises/06_dissemination/exercise2/ESTAT/app.R
@@ -1,15 +1,20 @@
+options(scipe=99999)
+
 library(tidyverse)
 library(shiny)
 library(eurostat)
 library(giscoR)
+library(sf)
+
+eurostat_database <- get_eurostat_toc() %>% filter(type %in% c("table", "dataset"))
+nuts <- giscoR::gisco_get_nuts(resolution = "20")
 
 
 ui <- fluidPage(
   
-    titlePanel("ADD TITLE"),
+    titlePanel("Geography of EU data"),
 
     sidebarLayout(
-      
       # input
       sidebarPanel(
         # checkbox
@@ -22,40 +27,113 @@ ui <- fluidPage(
           inputId = "selected_var",
           label = "Select a Eurostat dataset:",
           choices = NULL
+        ),
+        # dropdown
+        selectInput(
+          inputId = "selected_year",
+          label = "Select a year:",
+          choices = NULL
         )
     ),
-        
         # output
-        mainPanel(
-          "[plot placeholder]", plotOutput("map"),
-          br(),
-          br(),
-          "[table placeholder]", tableOutput("descriptives")
-        )
+        mainPanel(plotOutput("map"))
     )
 )
 
-eurostat_database <- get_eurostat_toc() 
-
 server <- function(input, output, session) {
-  
-  # workflow to update the choice of data based on the selected nuts level
+
+  # update dataset list based on NUTS level
+  # show only datasets for selected NUTS level
   observeEvent(input$selected_nuts_level, {
-    
+
     filtered_datasets <- eurostat_database %>%
-      filter(type %in% c("table", "dataset")) %>%
-      filter(str_detect(title, regex(input$selected_nuts_level, ignore_case = TRUE))
-      )
-    
-    choices <- filtered_datasets$title
-    
+      filter(str_detect(title, regex(input$selected_nuts_level, ignore_case = TRUE)))
+
     updateSelectInput(
       session,
       inputId = "selected_var",
-      choices = sort(choices),
-      selected = ""
+      choices = sort(filtered_datasets$title),
+      selected = NULL
     )
   })
+
+  selected_dataset_id <- reactive({
+    req(input$selected_var)
+
+    eurostat_database %>%
+      filter(title == input$selected_var) %>%
+      pull(code) %>%
+      first()
+  })
+
+  raw_data <- reactive({
+    req(selected_dataset_id())
+
+    get_eurostat(id = selected_dataset_id())
+  })
+
+  selected_levl_code <- reactive({
+    req(input$selected_nuts_level)
+    
+    case_when(
+      input$selected_nuts_level == "NUTS 1" ~ 1,
+      input$selected_nuts_level == "NUTS 2" ~ 2,
+      input$selected_nuts_level == "NUTS 3" ~ 3
+    )
+  })
+  
+  # update year selector from dataset
+  observeEvent(raw_data(), {
+
+    years <- raw_data() %>%
+      mutate(YEAR = substr(TIME_PERIOD, 1, 4)) %>%
+      pull(YEAR) %>%
+      unique() %>%
+      sort()
+
+    updateSelectInput(
+      session,
+      inputId = "selected_year",
+      choices = years)
+  })
+  
+
+  dataset_prepared <- reactive({
+    req(raw_data(), input$selected_year, selected_levl_code())
+    
+    raw_data() %>%
+      mutate(YEAR = substr(TIME_PERIOD, 1, 4)) %>%
+      left_join(nuts) %>%
+      # st_as_sf() %>%
+      filter(
+        YEAR == input$selected_year,
+        LEVL_CODE == selected_levl_code()
+      )
+  })
+  
+  nuts_filtered <- reactive({
+    req(selected_levl_code())
+    
+    nuts %>% filter(LEVL_CODE == selected_levl_code())
+  })
+  
+  map_data <- reactive({
+    req(dataset_prepared(), nuts_filtered())
+    
+    left_join(
+      nuts_filtered(),
+      dataset_prepared()
+    )
+  })
+  
+  output$map <- renderPlot({
+    req(map_data())
+    
+   plot(map_data()["values"], main="")
+    
+  })
+  
 }
+
 
 shinyApp(ui = ui, server = server)
