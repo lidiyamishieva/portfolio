53cb98683e5921824b58cd743e2d839e46a54435
lidiyamishieva <lidiya.mishieva@mail.de>
2026-02-18 22:48:06 +0100

    adjust folder structure to match the workfow

---
+++ 01_research_compendium/01_code/03_produce_direct_estimates.R
@@ -0,0 +1,89 @@
+# 00 setup -----
+
+# clean up workspace
+rm(list=ls())
+
+# load packages
+library(tidyverse)
+library(eurostat)
+library(sf)
+library(sae)
+
+# 01 import data -----
+
+# import analysis dataset (survey data)
+data_analysis <- read_csv("00_data/ii_processed/ess11_analysis_dataset.csv")
+
+# import population sizes from eurostat
+dataset_name <- c("Population on 1 January by age group, sex and NUTS 3 region")
+search_result <- search_eurostat(dataset_name, type = "dataset")
+dataset_id <- search_result$code[1]
+estat <- get_eurostat(dataset_id, time_format = "num", stringsAsFactors = TRUE)
+
+# 02 data transformations -----
+
+# min max scale the factor scores
+
+range01 <- function(x){(x-min(x))/(max(x)-min(x))}
+
+data_analysis <- data_analysis %>%
+  mutate(across(all_of(c("F1W", "F2W", "F3W", "F4W", "F5W")), ~ range01(.x ), .names = "{.col}_scaled"))
+
+# filter population sizes by year and age to match
+# the population surveyed in the ESS (individuals above 15)
+estat2 <- estat %>%
+  filter(!age %in% c("TOTAL", "UNK", "Y_LT5", "Y5-9", "Y10-14", "Y_GE85")) %>%
+  filter(sex == "T") %>% 
+  filter(TIME_PERIOD == 2023) %>%
+  dplyr::select(c("geo", "values")) %>%
+  rename(pop_size = values, region = geo) %>%
+  group_by(region) %>%
+  summarise(pop_size = sum(pop_size))
+
+# merge administrative data and survey data
+data_analysis <- data_analysis %>% left_join(estat2, by="region")
+
+# 03 produce direct estimates (Horvitz-Thompson) -----
+
+# collect sample sizes per area
+data_analysis <- data_analysis %>% group_by(region) %>% mutate(n = n())
+# adjust design weights to be the inverse of the first-order probability to be sampled in the area
+data_analysis$dweight_adj <- (data_analysis$pop_size/data_analysis$n)*data_analysis$dweight
+
+# create a data frame that contatins domain sizes for each region
+N <- data_analysis %>% 
+  group_by(region) %>% 
+  summarise(mean(pop_size)) %>% 
+  rename(pop_size = `mean(pop_size)`) %>% 
+  drop_na() %>%
+  as.data.frame()
+
+# produce direct estimates for all factors
+f1_weighted <- direct(y=data_analysis$F1W_scaled, sweight = data_analysis$dweight_adj, domsize = as.data.frame(N[, c("region", "pop_size")]), dom=data_analysis$region)
+f2_weighted <- direct(y=data_analysis$F2W_scaled, sweight = data_analysis$dweight_adj, domsize = as.data.frame(N[, c("region", "pop_size")]), dom=data_analysis$region)
+f3_weighted <- direct(y=data_analysis$F3W_scaled, sweight = data_analysis$dweight_adj, domsize = as.data.frame(N[, c("region", "pop_size")]), dom=data_analysis$region)
+f4_weighted <- direct(y=data_analysis$F4W_scaled, sweight = data_analysis$dweight_adj, domsize = as.data.frame(N[, c("region", "pop_size")]), dom=data_analysis$region)
+f5_weighted <- direct(y=data_analysis$F5W_scaled, sweight = data_analysis$dweight_adj, domsize = as.data.frame(N[, c("region", "pop_size")]), dom=data_analysis$region)
+
+# combine all estimates
+
+names(f1_weighted)[1] <- "region"
+names(f1_weighted)[3:5] <- paste0("F1_wgt_", names(f1_weighted)[3:5])
+
+for(i in 1:5){
+  df_w <- get(paste0("f", i, "_weighted"))
+  names(df_w)[1] <- "region"
+  names(df_w)[3:ncol(df_w)] <- 
+    paste0("F", i, "_wgt_", names(df_w)[3:ncol(df_w)])
+  assign(paste0("f", i, "_weighted"), df_w)
+}
+
+all_direct_estimates <- reduce(
+  list(f1_weighted, f2_weighted, f3_weighted, f4_weighted, f5_weighted),
+  function(x, y) bind_cols(x, y[, -c(1,2)])
+)
+
+# 04 export data -----
+
+write_csv(estat2, "00_data/ii_processed/pop_sizes_eu_2023.csv")
+write_csv(all_direct_estimates, "00_data/ii_processed/all_direct_estimates.csv")
---
+++ 01_research_compendium/01_code/04_produce_graphs.R
@@ -0,0 +1,140 @@
+# 00 setup -----
+
+# clean up workspace
+rm(list=ls())
+
+# load packages
+library(tidyverse)
+library(sf)
+library(giscoR)
+library(RColorBrewer)
+library(classInt)
+
+# 01 import data -----
+
+# import NUTS regions with geography information
+nuts <- bind_rows(
+  gisco_get_nuts(nuts_level = 1, year = 2021, resolution = "10", cache = TRUE, update_cache = TRUE) %>% mutate(level = 1),
+  gisco_get_nuts(nuts_level = 2, year = 2021, resolution = "10", cache = TRUE, update_cache = TRUE) %>% mutate(level = 2),
+  gisco_get_nuts(nuts_level = 3, year = 2021, resolution = "10", cache = TRUE, update_cache = TRUE) %>% mutate(level = 3))
+
+# import direct estimates
+all_direct_estimates <- read_csv("00_data/ii_processed/all_direct_estimates.csv")
+
+# 02 data transformations -----
+
+# add geometries to the data frame with regional estimates
+
+all_direct_estimates <- all_direct_estimates %>%
+  left_join(
+    nuts %>% 
+      rename(region = NUTS_ID) %>% 
+      dplyr::select(c(region, geometry))
+  ) %>%
+  st_as_sf()
+
+# 03 visualizations -----
+
+# function to produce a map with the color scale matching the quantiles
+# of the distribution of the variable to be mapped
+plot_quantile_map <- function(
+    sf_data,
+    var,
+    n = 3,
+    palette = "Blues",
+    main = NULL,
+    sub = NULL,
+    legend_title = NULL
+) {
+  # extract variable
+  x <- sf_data[[var]]
+  
+  # palette
+  pal <- colorRampPalette(brewer.pal(n-1, palette))(n)
+  
+  # quantile breaks
+  brks <- classIntervals(x, n = n, style = "quantile")
+  
+  # classify values
+  classes <- cut(x, breaks = brks$brks, include.lowest = TRUE)
+  
+  # default titles
+  if (is.null(main)) main <- var
+  if (is.null(legend_title)) legend_title <- var
+  
+  # plot map
+  plot(
+    st_geometry(sf_data),
+    #col = pal[as.numeric(classes)],
+    col = ifelse(is.na(classes), "grey80", pal[as.numeric(classes)]),
+    border = NA,
+    main = main,
+    sub = sub,
+  )
+  
+  # legend labels
+  legend_labels <- paste0(
+    format(round(brks$brks[-length(brks$brks)], 3), big.mark = ","),
+    " – ",
+    format(round(brks$brks[-1], 3), big.mark = ",")
+  )
+  
+  legend
+  legend(
+    "bottomleft",
+    legend = legend_labels,
+    fill = pal,
+    title = "Quantiles", #legend_title,
+    border = "white",
+    cex = 0.6,
+    bty = "n"
+  )
+}
+
+# 04 export graphs -----
+
+par(mfrow=c(1,1))
+
+png("02_output/i_graphs/plot_f1_est.png", width = 700, height = 700, units = "px", pointsize = 5, res=300)
+plot_quantile_map(all_direct_estimates, var = "F1_wgt_F1_wgt_Direct", palette = "Reds",  main = "F1: Interpersonal trust\n(direct estimate, mean)", n=10)
+dev.off()
+
+png("02_output/i_graphs/plot_f1_cv.png", width = 700, height = 700, units = "px", pointsize = 5, res=300)
+plot_quantile_map(all_direct_estimates, var = "F1_wgt_F1_wgt_CV", palette = "Reds",  main = "F1: Interpersonal trust\n(direct estimate, cv)", n=10)
+dev.off()
+
+png("02_output/i_graphs/plot_f2_est.png", width = 700, height = 700, units = "px", pointsize = 5, res=300)
+plot_quantile_map(all_direct_estimates, var = "F2_wgt_Direct", palette = "Reds",  main = "F2: Social relations\n(direct estimate, mean)", n=10)
+dev.off()
+
+png("02_output/i_graphs/plot_f2_cv.png", width = 700, height = 700, units = "px", pointsize = 5, res=300)
+plot_quantile_map(all_direct_estimates, var = "F2_wgt_CV", palette = "Reds",  main = "F2: Social relations\n(direct estimate, cv)", n=10)
+dev.off()
+
+png("02_output/i_graphs/plot_f3_est.png", width = 700, height = 700, units = "px", pointsize = 5, res=300)
+plot_quantile_map(all_direct_estimates, var = "F3_wgt_Direct", palette = "Reds",  main = "F3: Openness\n(direct estimate, mean)", n=10)
+dev.off()
+
+png("02_output/i_graphs/plot_f3_cv.png", width = 700, height = 700, units = "px", pointsize = 5, res=300)
+plot_quantile_map(all_direct_estimates, var = "F3_wgt_CV", palette = "Reds",  main = "F3: Openness\n(direct estimate, cv)", n=10)
+dev.off()
+
+png("02_output/i_graphs/plot_f4_est.png", width = 700, height = 700, units = "px", pointsize = 5, res=300)
+plot_quantile_map(all_direct_estimates, var = "F4_wgt_Direct", palette = "Reds",  main = "F4: Trust in institutions\n(direct estimate, mean)", n=10)
+dev.off()
+
+png("02_output/i_graphs/plot_f4_cv.png", width = 700, height = 700, units = "px", pointsize = 5, res=300)
+plot_quantile_map(all_direct_estimates, var = "F4_wgt_CV", palette = "Reds",  main = "F4: Trust in institutions\n(direct estimate, cv)", n=10)
+dev.off()
+
+png("02_output/i_graphs/plot_f5_est.png", width = 700, height = 700, units = "px", pointsize = 5, res=300)
+plot_quantile_map(all_direct_estimates, var = "F5_wgt_Direct", palette = "Reds",  main = "F5: Legitimacy of institutions\n(direct estimate, mean)", n=10)
+dev.off()
+
+png("02_output/i_graphs/plot_f5_cv.png", width = 700, height = 700, units = "px", pointsize = 5, res=300)
+plot_quantile_map(all_direct_estimates, var = "F5_wgt_CV", palette = "Reds",  main = "F5: Legitimacy of institutions\n(direct estimate, cv)", n=10)
+dev.off()
+
+# 05 export data -----
+
+st_write(all_direct_estimates, "00_data/ii_processed/all_direct_estimates.geojson", append = FALSE)
